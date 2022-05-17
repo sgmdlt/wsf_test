c710ac30e42ffe2f16634a29ac01dcaeef8839f3
Your Name <you@example.com>
2022-05-18 03:30:05 +0400

    add images saving

---
+++ scraper/storage.py
@@ -0,0 +1,41 @@
+import logging
+import os
+from random import random, sample
+import string
+import traceback
+from urllib.parse import urlparse
+
+from scraper.exceptions import FileSystemError
+
+logger = logging.getLogger(__name__)
+
+
+def local_name(name, ext=''):
+
+    path = _generate(name)
+
+    return '{0}{1}'.format(path, ext)
+
+
+def _generate(name):
+    name = name.strip()
+    if name:
+        return name.replace('/', '-').replace(' ', '-')
+    return 'noname_{}'.format(_random_name())
+
+
+def _random_name():
+    digits_letters = string.ascii_letters + string.digits
+    return ''.join(sample(digits_letters, 6))
+
+
+def save(source, path, mode='wb'):
+    try:
+        with open(path, mode=mode) as out:
+            out.write(source)
+    except IOError as exc:
+        logger.debug(traceback.format_exc(2, chain=False))
+        logger.error('No permission to directory')
+        raise FileSystemError() from exc
+    logger.info('{0} was saved successfully'.format(path))
+    return path
