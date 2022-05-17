0df2a7dd52b6ca15cdf465883d35411887cdecd2
Your Name <you@example.com>
2022-05-18 03:31:02 +0400

    add files renaming

---
+++ scraper/rename.py
@@ -0,0 +1,43 @@
+import os
+
+from scraper.io import get_data, get_files, filter_persons
+
+
+def run(directory, names_source):
+    files_with_meta = get_files(directory)
+    persons = filter_persons(get_data(names_source))
+    filenames = rename(files_with_meta, persons)
+    rewrite(filenames)
+    return directory
+
+
+def rename(filenames, persons):
+    
+    for file in filenames:
+        for person in persons:
+            if file['last_name'] in person:
+                person_id = persons[person].get('person_id')
+                section_id = persons[person].get('section_id')
+
+                if person_id and person_id != 'NULL':
+                    new_name = person_id
+                else:
+                    new_name = 'section_{0}'.format(section_id)
+
+                new_path = _change_path(file['path'], new_name)
+                file['new_path'] = new_path
+
+    return filenames
+    
+
+def _change_path(old_path, new_name):
+    parent_dir = os.path.abspath(os.path.join(old_path, os.pardir))
+    return os.path.join(parent_dir, new_name)
+
+
+def rewrite(files):
+    for file in files:
+        old_path = file.get('path')
+        new_path = file.get('new_path')
+        if new_path:
+            os.rename(old_path, '{0}.jpg'.format(new_path))
